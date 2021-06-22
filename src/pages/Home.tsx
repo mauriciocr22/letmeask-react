2511a86b9d11492877102fe957b04fb23172f492
Maurício de Carvalho <mauriciocr223@gmail.com>
2021-06-22 19:17:42 -0300

    feat: autenticação com firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { useHistory } from 'react-router-dom'
 
+import { auth, firebase } from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -12,7 +14,13 @@ export function Home() {
   const history = useHistory();
 
   function handleCreateRoom() {
-    history.push('/rooms/new')
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result);
+    })
+
+    history.push('/rooms/new');
   }
 
   return(
